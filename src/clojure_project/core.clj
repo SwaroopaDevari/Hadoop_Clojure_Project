1ade91f8e1d525c64075f48c1f6761193fe4ce60
Vinay Devari <vinaydevari@Vinays-MBP.zyxel.com>
2019-06-06 19:59:56 -0500

    changed

--- src/clojure_project/core.clj
+++ src/clojure_project/core.clj
@@ -4,3 +4,7 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, Vedu"))
+
+(def ord_status ["CLO" "opened" "pending123" "CompleTed 234"])
+(filter #(re-matches #"[A-Z]{3}" % )
+          ord_status)
